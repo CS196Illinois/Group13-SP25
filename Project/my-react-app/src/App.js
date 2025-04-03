10310236bfe40e646287963a3a5b82ea95ffba8c
benleyv2 <benleyv2@illinois.edu>
2025-04-03 14:17:45 -0500

    hello

--- Project/my-react-app/src/App.js
+++ Project/my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and benley reload.
+          Edit <code>src/App.js</code> and benley reload hello hello.
         </p>
         <a
           className="App-link"
